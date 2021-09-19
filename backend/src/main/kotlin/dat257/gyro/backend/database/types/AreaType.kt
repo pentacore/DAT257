40e22208137501f17767074b28e5a141dcb4376f
Martin Claesson <martin@claesson.co>
2021-09-19 19:33:59 +0200

    Revert "Reflections"

--- backend/src/main/kotlin/dat257/gyro/backend/database/types/AreaType.kt
+++
@@ -1,9 +0,0 @@
-package dat257.gyro.backend.database.types
-
-enum class AreaType(val type: String) {
-    PARK("park"),
-    CITY("city"),
-    LANDSCAPE("landscape"),
-    FOREST("forest"),
-    MOUNTAIN("mountain")
-}
--- backend/src/main/kotlin/dat257/gyro/backend/database/types/ShowOnMap.kt
+++
@@ -1,7 +0,0 @@
-package dat257.gyro.backend.database.types
-
-enum class ShowOnMap(val value: String) {
-    YES("yes"),
-    NO("no"),
-    FRIENDS("friends")
-}
