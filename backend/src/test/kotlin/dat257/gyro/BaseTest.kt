ad8c3d13b10d817acbd64cdb3a174dbc81d78528
penta <martin@claesson.co>
2021-09-22 15:25:25 +0200

    2.1.3 - Added tests to the CI action, rename build action to CI, remove unused import

--- backend/src/test/kotlin/dat257/gyro/BaseTest.kt
+++ backend/src/test/kotlin/dat257/gyro/BaseTest.kt
@@ -2,7 +2,6 @@ package dat257.gyro
 
 import dat257.gyro.backend.api.Routes
 import dat257.gyro.backend.database.DatabaseClient
-import org.junit.Before
 
 open class BaseTest {
     protected val app = Routes.routes
--- backend/src/test/kotlin/dat257/gyro/api/PingPongTests.kt
+++ backend/src/test/kotlin/dat257/gyro/api/PingPongTests.kt
@@ -5,16 +5,18 @@ import org.http4k.core.Method
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class ServerPingPongTests: BaseTest() {
+class PingPongTests: BaseTest() {
 
     @Test
     fun testServerPing() {
+        println("Running Ping Test")
         val response = app(org.http4k.core.Request(Method.GET, "/ping"))
         assertEquals("pong", response.bodyString())
     }
 
     @Test
     fun testServerPong() {
+        println("Running Pong Test")
         val response = app(org.http4k.core.Request(Method.GET, "/pong"))
         assertEquals("ping", response.bodyString())
     }
