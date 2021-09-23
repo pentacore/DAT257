022c639bfcfe14b0527f964bc6727e9184c5d0a9
emps <35624213+smeppen@users.noreply.github.com>
2021-09-23 10:48:46 +0200

    Added button to go from MainActivity to MapActivity

--- app/src/main/kotlin/dat257/gyro/MainActivity.kt
+++ app/src/main/kotlin/dat257/gyro/MainActivity.kt
@@ -1,11 +1,17 @@
 package dat257.gyro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startMapActivity(view: View) {
+        startActivity(Intent(this, MapActivity::class.java))
     }
 }
