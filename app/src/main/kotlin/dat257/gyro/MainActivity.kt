4c0c4295ee332b3d2f25b72048cca3d8e02b18f8
emps <35624213+smeppen@users.noreply.github.com>
2021-09-23 12:11:54 +0200

    Amended some egregious xml practices

--- app/src/main/kotlin/dat257/gyro/MainActivity.kt
+++ app/src/main/kotlin/dat257/gyro/MainActivity.kt
@@ -3,15 +3,16 @@ package dat257.gyro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.mapButton).setOnClickListener { startMapActivity() }
     }
 
-    fun startMapActivity(view: View) {
+    private fun startMapActivity() {
         startActivity(Intent(this, MapActivity::class.java))
     }
 }
