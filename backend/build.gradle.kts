1ff3093623639e36b886d3cfe4c3ba47354239ec
Martin Claesson <martin@claesson.co>
2021-09-30 14:48:34 +0200

    Update dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -9,17 +9,17 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     implementation(project(":shared"))
-    implementation(platform("org.http4k:http4k-bom:4.13.0.0"))
-    implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("org.jetbrains.exposed:exposed-core:0.34.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.34.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.34.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.34.1")
-    implementation("org.postgresql:postgresql:42.2.2")
-    implementation("com.h2database:h2:1.4.199")
-    implementation("org.http4k:http4k-core")
-    implementation("org.http4k:http4k-server-netty")
-    implementation("org.http4k:http4k-client-apache")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("org.jetbrains.exposed:exposed-core:0.35.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.34.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.34.2")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.34.2")
+    implementation("org.postgresql:postgresql:42.2.23.jre7")
+    implementation("com.h2database:h2:1.4.200")
+    implementation(platform("org.http4k:http4k-bom:4.13.1.0"))
+    implementation("org.http4k:http4k-core:4.13.1.0")
+    implementation("org.http4k:http4k-client-apache:4.13.1.0")
+    implementation("org.http4k:http4k-server-netty:4.13.1.0")
     testImplementation(kotlin("test"))
 }
 
