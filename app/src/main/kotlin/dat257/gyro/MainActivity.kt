dcc9648dd36a9bfcd83bb4a6c77428a1571fc473
emps <35624213+smeppen@users.noreply.github.com>
2021-09-30 16:53:31 +0200

    Added basic navigation bar that switches between hello-world-esque fragments.

--- app/src/main/kotlin/dat257/gyro/MainActivity.kt
+++ app/src/main/kotlin/dat257/gyro/MainActivity.kt
@@ -1,18 +1,39 @@
 package dat257.gyro
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.mapButton).setOnClickListener { startMapActivity() }
+
+        val helloWorldFragment = Fragment(R.layout.fragment_hello_world)
+        val helloMapFragment = Fragment(R.layout.fragment_hello_map)
+        val helloSettingsFragment = Fragment(R.layout.fragment_hello_settings)
+
+        setCurrentFragment(helloWorldFragment)
+
+        findViewById<NavigationBarView>(R.id.bottomNavigation).setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item_home -> setCurrentFragment(helloWorldFragment)
+                R.id.item_map -> setCurrentFragment(helloMapFragment)
+                R.id.item_settings -> setCurrentFragment(helloSettingsFragment)
+            }
+            true
+        }
+
     }
 
-    private fun startMapActivity() {
-        startActivity(Intent(this, MapActivity::class.java))
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fcv_main, fragment)
+            commit()
+        }
     }
+
 }
