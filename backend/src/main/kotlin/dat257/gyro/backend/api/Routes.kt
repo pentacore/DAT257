6232619c49c03ad21cbc3d96d133bd49a0648a11
Martin Claesson <martin@claesson.co>
2021-09-30 18:17:59 +0200

    Import the methods to make it more clean

--- backend/src/main/kotlin/dat257/gyro/backend/api/Routes.kt
+++ backend/src/main/kotlin/dat257/gyro/backend/api/Routes.kt
@@ -3,21 +3,21 @@ package dat257.gyro.backend.api
 import dat257.gyro.backend.api.methods.WalkPathEndpoint
 import dat257.gyro.backend.api.methods.ping
 import dat257.gyro.backend.api.methods.pong
-import org.http4k.core.Method
+import org.http4k.core.Method.*
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 
 class Routes {
     companion object {
         val routes = routes(
-            "ping" bind Method.GET to { ping() },
-            "pong" bind Method.GET to { pong() },
+            "ping" bind GET to { ping() },
+            "pong" bind GET to { pong() },
             "api" bind routes(
                 "walkpath" bind routes(
-                    Method.GET to { req -> WalkPathEndpoint.get(req) },
-                    Method.PUT to { req ->WalkPathEndpoint.put(req) },
-                    Method.PATCH to { req ->WalkPathEndpoint.patch(req) },
-                    Method.DELETE to { req ->WalkPathEndpoint.delete(req) }
+                    GET to { req -> WalkPathEndpoint.get(req) },
+                    PUT to { req ->WalkPathEndpoint.put(req) },
+                    PATCH to { req ->WalkPathEndpoint.patch(req) },
+                    DELETE to { req ->WalkPathEndpoint.delete(req) }
                 )
             )
         )
