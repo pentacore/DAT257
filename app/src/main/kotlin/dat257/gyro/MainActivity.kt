36e33eb17e1cccc4de2d7b7747d11e07bea088f3
Martin Claesson <martin@claesson.co>
2021-10-04 18:30:27 +0200

    Add files via upload
    Fixade settings meny samt ändrade setCurrentfragment så man kan gömma timer

--- app/src/main/kotlin/dat257/gyro/MainActivity.kt
+++ app/src/main/kotlin/dat257/gyro/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationBarView
+import org.osmdroid.views.MapView
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val helloWorldFragment = Fragment(R.layout.fragment_hello_world)
         val helloMapFragment = MapFragment()
         val helloSettingsFragment = Fragment(R.layout.fragment_hello_settings)
+        val settingsFragment = SettingsFragment()
 
         setCurrentFragment(helloWorldFragment)
 
@@ -22,17 +24,44 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId) {
                 R.id.item_home -> setCurrentFragment(helloWorldFragment)
                 R.id.item_map -> setCurrentFragment(helloMapFragment)
-                R.id.item_settings -> setCurrentFragment(helloSettingsFragment)
+                R.id.item_settings -> setCurrentFragment(settingsFragment, true)
             }
             true
         }
 
+
     }
 
-    private fun setCurrentFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fcv_main, fragment)
-            commit()
+    private fun setCurrentFragment(fragment: Fragment, hideTimer:Boolean = false) {
+
+        var isSame = false
+        if(supportFragmentManager.findFragmentById(R.id.fcv_main) == fragment)
+        {  isSame = true }
+
+
+        if(!isSame) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fcv_main, fragment)
+                commit()
+            }
+
+            var isHidden = true
+            val x = supportFragmentManager.findFragmentById(R.id.Timerfragment)
+            if (x != null){isHidden = x.isHidden}
+
+
+            if(hideTimer && !isHidden){
+                if (x != null) {
+                    supportFragmentManager.beginTransaction().apply { hide(x)
+                    commit()}
+                }
+            }
+            else if(isHidden && !isSame){
+            if (x != null) {
+                supportFragmentManager.beginTransaction().apply{ show(x)
+                    commit() }
+            }
+            }
         }
     }
 }
---
+++ app/src/main/kotlin/dat257/gyro/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package dat257.gyro
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
