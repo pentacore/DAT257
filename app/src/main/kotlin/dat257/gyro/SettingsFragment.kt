e03a2cd4653ec3e41934e70dea8bae58844b01e6
Sinestral <felholmstrom@gmail.com>
2021-10-11 18:34:14 +0200

    Added more setting-options and relevant arrays/strings.

--- app/src/main/kotlin/dat257/gyro/SettingsFragment.kt
+++ app/src/main/kotlin/dat257/gyro/SettingsFragment.kt
@@ -1,13 +1,13 @@
 package dat257.gyro
 
+import android.content.BroadcastReceiver
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import android.content.Intent
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 
         setPreferencesFromResource(R.xml.preference_screen, rootKey)
-
-
     }
 }
