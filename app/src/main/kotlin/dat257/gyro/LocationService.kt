1b43ba3292342706cb1bfaa1c7abdea77f5841ab
erikgj <erikgj@student.chalmers.se>
2021-10-12 17:48:55 +0200

    Implemented fusedprovider in the location service

--- app/src/main/kotlin/dat257/gyro/LocationService.kt
+++ app/src/main/kotlin/dat257/gyro/LocationService.kt
@@ -6,11 +6,11 @@ import android.content.Intent
 import android.location.Location
 import android.location.LocationListener
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.Task
 import dat257.gyro.patterns.publisherSubscriber.ChannelName
 import dat257.gyro.patterns.publisherSubscriber.Message
 import dat257.gyro.patterns.publisherSubscriber.Publisher
@@ -19,15 +19,18 @@ import dat257.gyro.patterns.publisherSubscriber.Publisher
  * @author Jonathan
  * källa för service: https://dzone.com/articles/how-to-create-a-background-service-in-android
  */
-class LocationService: Service(), Publisher {
+class LocationService : Service(), Publisher {
     override fun onBind(p0: Intent?): IBinder? = null
+
     //Coordinates
     private lateinit var locationClient: FusedLocationProviderClient
     private val refreshDistance: Float = 1.01f // exempel vet inte enhet
     private val refreshTime: Long = 1
     private lateinit var locationListener: LocationListener
+
     /**
      * @author Jonathan
+     * @author Erik
      */
     @SuppressLint("MissingPermission")
     override fun onCreate() {
@@ -36,20 +39,50 @@ class LocationService: Service(), Publisher {
         PermissionHandler.location(this)
         locationClient = LocationServices.getFusedLocationProviderClient(this)
         locationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                if(location != null) {
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
                     onLocationUpdate(location)
                 }
             }
-        locationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, null)
+        val locationRequest = createLocationRequest()
+        //locationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, null)
+        /*
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+
+        task.addOnSuccessListener { locationSettingsResponds ->
+*/
+        val locationCallback = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult){
+                onLocationUpdate(locationResult.locations.last())
+            }
+        }
+        locationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()!!
+        )
+    }
+
+    /**
+     * @author Erik
+     */
+    private fun createLocationRequest(): LocationRequest {
+        return LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
     }
 
     /**
      * @author Jonathan
      */
-
-    private fun onLocationUpdate(location: Location) = with(Message(location)){
-        Log.d("LocationService","Location updated")
-        publish(ChannelName.Location,this)
+    public fun onLocationUpdate(location: Location) = with(Message(location)) {
+        Log.d("LocationService", "Location updated")
+        publish(ChannelName.Location, this)
     }
 }
