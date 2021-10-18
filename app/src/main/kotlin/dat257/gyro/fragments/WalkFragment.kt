dfbd574cff347ad4358b35d5c36c12c40483d5ef
JessicaBarai <mejlbarai@gmail.com>
2021-10-18 22:52:52 +0200

    Cleaned up in walkFragment, time and distance is now shown in walkFragment.

--- app/src/main/kotlin/dat257/gyro/fragments/WalkFragment.kt
+++ app/src/main/kotlin/dat257/gyro/fragments/WalkFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import dat257.gyro.R
 import dat257.gyro.model.RecordingControllerInstruction
 import dat257.gyro.patterns.publisherSubscriber.ChannelName
@@ -22,9 +23,6 @@ import dat257.gyro.services.TimerService
 import kotlin.math.roundToInt
 
 class WalkFragment : Fragment(), Publisher, Subscriber {
-    //The channel that this publishes to is currently being made in LocationService. This should
-    //Probably change to be a central channel creator - or by changing the pattern.
-    //  private lateinit var walkMinimize: Button
     private lateinit var walkPlay: Button
     private lateinit var walkReset: Button
     private var timerStarted = false
@@ -36,27 +34,23 @@ class WalkFragment : Fragment(), Publisher, Subscriber {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_walk, container, false)
-        // walkMinimize = view.findViewById(R.id.walkMinimize)
-        // walkMinimize.setBackgroundResource(R.drawable.ic_baseline_minimize_24)
         walkPlay = view.findViewById(R.id.startStopButton)
-        walkPlay.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
+        walkPlay.setBackgroundResource(R.drawable.ic_baseline_play_circle_24)
         walkReset = view.findViewById(R.id.resetButton)
-        walkReset.setBackgroundResource(R.drawable.ic_baseline_refresh_24)
+        walkReset.setBackgroundResource(R.drawable.ic_baseline_stop_circle_24)
         subscribe(ChannelName.Distance)
         walkPlay.setOnClickListener {
             childFragmentManager.beginTransaction().apply {
-                Log.i("yaay", "snel hest")
                 timerStarted = !timerStarted
-                // walkButton.setBa
                 if (timerStarted) {
-                    walkPlay.setBackgroundResource(R.drawable.ic_baseline_pause_24)
+                    walkPlay.setBackgroundResource(R.drawable.ic_baseline_pause_circle_24)
                     publish(
                         ChannelName.RecordingControl,
                         Message(RecordingControllerInstruction.Play)
                     )
                     startTimer()
                 } else {
-                    walkPlay.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
+                    walkPlay.setBackgroundResource(R.drawable.ic_baseline_pause_circle_24)
                     publish(
                         ChannelName.RecordingControl,
                         Message(RecordingControllerInstruction.Pause)
@@ -69,6 +63,8 @@ class WalkFragment : Fragment(), Publisher, Subscriber {
             walkReset.setOnClickListener {
                 publish(ChannelName.RecordingControl, Message(RecordingControllerInstruction.Stop))
                 resetTimer()
+                timerStarted=false
+                walkPlay.setBackgroundResource(R.drawable.ic_baseline_play_circle_24)
             }
         }
 
@@ -80,18 +76,29 @@ class WalkFragment : Fragment(), Publisher, Subscriber {
     }
 
     override fun onUpdate(source: ChannelName, message: Message<*>) {
-        Log.i("In Walkfragmentupdate:", message.payload.toString())
-        Log.i("In Walkfragmentupdate:", getTimeStringFromDouble(time))
-        updateDistanceString()
+        when (source) {
+            ChannelName.Distance -> updateDistanceString(message.payload as Double)
+            else -> Log.e("MapFragment/onUpdate: ", "Unimplemented Channelsource")
+        }
+        updateDistanceString(message.payload as Double)
     }
 
-    fun updateDistanceString() {
+    fun updateDistanceString(d: Double) {
         //TODO: updateDistanceString
+        val distance = String.format("%.3f", d)
+        view?.findViewById<TextView>(R.id.distance_string)?.text = distance
     }
 
+
+    fun updateTimerString(){
+        view?.findViewById<TextView>(R.id.time_string)?.text=getTimeStringFromDouble(time)
+
+
+    }
     fun resetTimer() {
         stopTimer()
         time = 0.0
+        updateTimerString()
     }
 
     fun startStopTimer() {
@@ -105,34 +112,22 @@ class WalkFragment : Fragment(), Publisher, Subscriber {
     fun startTimer() {
         serviceIntent.putExtra(TimerService.TIME_EXTRA, time)
         activity?.startService(serviceIntent)
-        //view?.findViewById<com.google.android.material.button.MaterialButton>(R.id.startStopButton)?.text = "stop"
-        //binding.startStopButton.text = "Stop"
-        //binding.startStopButton.icon = getDrawable(R.drawable.ic_baseline_pause_24)
-        // HEJ JAG HETER JONATHAN
         view?.findViewById<com.google.android.material.button.MaterialButton>(R.id.startStopButton)
-            ?.setBackgroundResource(R.drawable.ic_baseline_pause_24)
-        //mainActivity.mapFragmentInfo.isRecording=true
-        //timerStarted = true
+            ?.setBackgroundResource(R.drawable.ic_baseline_pause_circle_24)
+
     }
 
     @SuppressLint("SetTextI18n", "UseCompatLoadingForDrawables")
     fun stopTimer() {
-        // stopService(serviceIntent)
         activity?.stopService(serviceIntent)
-        //view?.findViewById<com.google.android.material.button.MaterialButton>(R.id.startStopButton)?.text = "start"
         view?.findViewById<com.google.android.material.button.MaterialButton>(R.id.startStopButton)
-            ?.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24)
-        // binding.startStopButton.text = "Start"
-        //binding.startStopButton.icon = getDrawable(R.drawable.ic_baseline_play_arrow_24)
-        //mainActivity.mapFragmentInfo.isRecording=false
-        //timerStarted = false
+            ?.setBackgroundResource(R.drawable.ic_baseline_play_circle_24)
     }
 
     private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             time = intent.getDoubleExtra(TimerService.TIME_EXTRA, 0.0)
-            //view?.findViewById<TextView>(R.id.timeTV)?.text = getTimeStringFromDouble(time)
-            // binding.timeTV.text = getTimeStringFromDouble(time)
+            updateTimerString()
         }
     }
 
